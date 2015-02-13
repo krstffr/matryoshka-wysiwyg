6656e9aa331e980682e5b611ffa8a2af7c3248e3
Kristoffer Klintberg <krstffr@gmail.com>
2015-02-13 11:34:03 +0100

    Version bump 0.2.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   summary: 'Wysiwyg support for Matryoshka.',
   name: 'krstffr:matryoshka-wysiwyg',
   git: "https://github.com/krstffr/meteor-matryoshka-wysiwyg.git",
-  version: '0.2.1'
+  version: '0.2.2'
 });
 
 Package.onUse(function ( api ) {
